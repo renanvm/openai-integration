7d13150335b1f86bfb51681e0c7016a1490cc608
Renan Martins <renan@renan>
2023-08-21 14:40:11 -0300

    First commit

---
+++ src/main/kotlin/br/com/renan/openaiintegration/OpenaiIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package br.com.renan.openaiintegration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenaiIntegrationApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenaiIntegrationApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/renan/openaiintegration/OpenaiIntegrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.renan.openaiintegration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenaiIntegrationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
